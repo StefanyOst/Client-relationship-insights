e0567508972e3a960fd78b907d780f5edc0717df
Stefany Ost <stefanyregina678@gmail.com>
2025-08-26 11:24:30 -0300

    isGroup/Id's

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const raw = $json;
+const dadosWebhook = $json.body ?? $json;
 
-// Usando função para tentar pegar um valor em caminhos diferentes
-function pick(path, obj, def = "") {
-  return (
-    path.split(".").reduce(o && o[k] !== undefined ? o[k] : undefined, obj) ??
-    def
-  );
-}
+// verificando se é grupo
+const ehGrupo = Boolean(
+  dadosWebhook.isGroup ?? dadosWebhook.chat?.isGroup ?? false
+);
 
-// Nome do Grupo
-const group_name =
-  raw.group_name || raw.chatName || pick("messages.0.group_name", raw) || "";
-
-// ID do remetente (nmr de telefone no WhatsApp)
-const sender_id =
-  raw.sender_id || raw.from || pick("messages.0.from", raw) || "";
+// Id's
+const groupId =
+  dadosWebhook.chatId ?? dadosWebhook.from ?? dadosWebhook.chat?.id ?? "";
+const senderId =
+  dadosWebhook.participant ??
+  dadosWebhook.sender?.id ??
+  dadosWebhook.from ??
+  "";
